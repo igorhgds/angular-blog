01a51f43aad32be38526d5402226289b29b179cc
Igor Henrique <ihenrique@sp.senai.br>
2025-12-10 19:46:39 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+   {
+      "id": "1",
+      "photoCover": "https://m.media-amazon.com/images/S/pv-target-images/6abe2bf8f2376da1c5f5f29cdbce25aa58bf985456e2c4f0ff78d7e2ed8730b6._SX1080_FMjpg_.jpg",
+      "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+      "description": "Marvel Studios anuncia novo filme do homem de ferro"
+   },
+   {
+      "id": "2",
+      "photoCover": "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png",
+      "title": "Novo filme",
+      "description": "Descrição do novo filme"
+   },
+   {
+      "id": "3",
+      "photoCover": "https://c.files.bbci.co.uk/D1A2/production/_113566635_netflix02.jpg",
+      "title": "Novo filme",
+      "description": "Descrição do novo filme"
+   },
+   {
+      "id": "4",
+      "photoCover": "https://rollingstone.com.br/wp-content/uploads/homem_de_ferro_reproducao.jpg",
+      "title": "Novo filme",
+      "description": "Descrição do novo filme"
+   }
+]
